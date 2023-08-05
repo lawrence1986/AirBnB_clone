7662f07a88e57251f57b02420a99ebbcb1bbad37
lawrence1986 <madusquare@gmail.com>
2023-08-05 06:59:28 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -10,15 +10,15 @@ from models import store
 
 class BaseModel:
 
-    def __init__(self, *ptr, **kwptr):
-        if kwptr != None and kwptr != {}:
-            for entry in kwptr:
+    def __init__(self, *args, **kwargs):
+        if kwargs != None and kwargs != {}:
+            for entry in kwargs:
                 if entry == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(kwptr["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif entry == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(kwptr["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.__dict__[entry] = kwptr[entry]
+                    self.__dict__[entry] = kwargs[entry]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -40,4 +40,3 @@ class BaseModel:
         is_dict["created_at"] = is_dict["created_at"].isoformat()
         is_dict["updated_at"] = is_dict["updated_at"].isoformat()
         return is_dict
-
