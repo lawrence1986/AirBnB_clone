77caad5a80f497c368fdf3b31c0c47f2f1eed03e
codeabuu <abdulngui718@gmail.com>
2023-08-06 14:11:32 -0700

    updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,10 +21,11 @@ class FileStorage:
         """Generates a key for the object,adds it to objects dict"""
         class_name = type(obj).__name__
         key = "{}.{}".format(class_name, obj.id)
-        obj = fso[key]
+        obj = FileStorage.__objects[key]
 
     def save(self):
         """Saves the objects dictionary to a JSON file"""
+        fso = FileStorage.__objects
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             d = {k: v.to_dict() for k, v in fso.items()}
             json.dump(d, f)
@@ -38,7 +39,7 @@ class FileStorage:
         with open(file_path, "r", encoding="utf-8") as f:
             obj_dict = json.load(f)
             obj_dict = {k: self.classes()[v["__class__"]](**v) for k, v in obj_dict.items()}
-            obj_dict = fso
+            obj_dict = FileStorage.__objects
 
     def classes(self):
         """Returns a dictionary of valid classes and their references"""
@@ -50,58 +51,49 @@ class FileStorage:
         from models.place import Place
         from models.review import Review
 
-        classes = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Place": Place,
-            "Review": Review
-        }
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
         return classes
 
     def attributes(self):
         """Defines attributes and their types for each class"""
         attributes = {
-            "BaseModel": {
-                "id": str,
-                "created_at": datetime.datetime,
-                "updated_at": datetime.datetime
-            },
-            "User": {
-                "email": str,
-                "password": str,
-                "first_name": str,
-                "last_name": str
-            },
-            "State": {
-                "name": str
-            },
-            "City": {
-                "state_id": str,
-                "name": str
-            },
-            "Amenity": {
-                "name": str
-            },
-            "Place": {
-                "city_id": str,
-                "user_id": str,
-                "name": str,
-                "description": str,
-                "number_rooms": int,
-                "number_bathrooms": int,
-                "max_guest": int,
-                "price_by_night": int,
-                "latitude": float,
-                "longitude": float,
-                "amenity_ids": list
-            },
-            "Review": {
-                "place_id": str,
-                "user_id": str,
-                "text": str
-            }
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
         }
         return attributes
