a866de9cd31e49ca3db1e7b2ecb27f95224c8951
codeabuu <abdulngui718@gmail.com>
2023-08-06 13:40:46 -0700

    updates

--- console.py
+++ console.py
@@ -146,6 +146,74 @@ class HBNBCommand(cmd.Cmd):
                 class_name + ".")]
             print(len(matches))
 
+    def _precmd(self, arg):
+        """Intercepts commands to test for class.syntax()"""
+        match = re.search(r"^(\w*)\.(\w+)(?:\(([^)]*)\))$", arg)
+        if not match:
+            return arg
+
+        classname, method, args = match.groups()
+        uid, attr_or_dict = extract_uid_and_args(args)
+
+        attr_and_value = ""
+
+        if method == "update" and attr_or_dict:
+            update_dict_match = re.search('^({.*})$', attr_or_dict)
+            if update_dict_match:
+                self.update_dict(classname, uid, update_dict_match.group(1))
+                return ""
+
+            attr_and_value = extract_attr_and_value(attr_or_dict)
+
+            command = f"{method} {classname} {uid} {attr_and_value}"
+            self.onecmd(command)
+            return command
+
+        def extract_uid_and_args(args):
+
+            match_uid_and_args = re.search('^"([^"]*)"(?:, (.*))?$', args)
+            if match_uid_and_args:
+                uid = match_uid_and_args.group(1)
+                attr_or_dict = match_uid_and_args.group(2)
+            else:
+                uid = args
+                attr_or_dict = False
+
+                return uid, attr_or_dict
+
+        def extract_attr_and_value(attr_or_dict):
+            match_attr_and_value = re.search(
+            '^(?:"([^"]*)")?(?:, (.*))?$', attr_or_dict)
+
+            if match_attr_and_value:
+                attr_and_value = (match_attr_and_value.group(1) or "") + " " + (match_attr_and_value.group(2) or "")
+            else:
+                attr_and_value = ""
+
+                return attr_and_value
+
+        def update_dict(self, classname, uid, s_dict):
+            """Helper method for update() with a dictionary."""
+            s = s_dict.replace("'", '"')
+            d = json.loads(s)
+            if not classname:
+                print("** class name missing **")
+            elif classname not in storage.classes():
+                print("** class doesn't exist **")
+            elif uid is None:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(classname, uid)
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    attributes = storage.attributes()[classname]
+                    for attribute, value in d.items():
+                        if attribute in attributes:
+                            value = attributes[attribute](value)
+                            setattr(storage.all()[key], attribute, value)
+                            storage.all()[key].save()
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
